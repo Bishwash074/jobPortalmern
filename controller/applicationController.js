d9654efd37a7030b7cc4e7a916017d8207d4e957
Bishwash Dhital <bishwashdhital73@gmail.com>
2026-01-17 11:06:25 +0545

    enchaces application api

--- controller/applicationController.js
+++ controller/applicationController.js
@@ -1,54 +1,91 @@
+const { where } = require("sequelize");
 const { Application } = require("../model");
 
-const jobApply=async(req,res)=>{
-  const {jobId}=req.params;
-  const userId=req.user.id;
+const jobApply = async (req, res) => {
+  const { jobId } = req.params;
+  const userId = req.user.id;
 
-  const application=await Application.create({
-    jobId:jobId,
-    userId:userId
+  const application = await Application.create({
+    jobId: jobId,
+    userId: userId
   })
 
   return res.status(200).json({
-    messsage:"job applied sucessfully",
+    messsage: "job applied sucessfully",
     application
   })
 }
 
 
-const updateApplicationStatus=async(req,res)=>{
-  const {id}=req.params
-  const {status}=req.body
-  const application=await Application.findByPk(id)
+const updateApplicationStatus = async (req, res) => {
+  const { id } = req.params
+  const { status } = req.body
+  const application = await Application.findByPk(id)
 
-  if(!application){
+  if (!application) {
     return res.status(400).json({
-      messsage:"Application not found"
+      messsage: "Application not found"
     })
   }
 
-  const updateApplication=await Application.update({
-    status:status
-  })
+  const updateApplication = await Application.update({
+    status: status
+  },{
+where:{id}})
 
   return res.status(200).json({
-    messsage:"Application status updated sucessfully",
+    messsage: "Application status updated sucessfully",
     updateApplication
   })
 }
 
-const deleteApplication=async (req,res)=>{
-  const {id}=req.params;
-  const application=await Application.findByPk(id);
+const deleteApplication = async (req, res) => {
+  const { id } = req.params;
+  const application = await Application.findByPk(id);
 
+  if (!application) {
+    return res.status(404).json({
+      message: "Application not found"
+    })
+  }
+
+  await Application.destroy({where:{
+    id
+  }})
+  return res.status(200).json({
+    message: "Application deleted successfully",
+
+  });
+}
+
+const getApplication = async (req, res) => {
+  const application=await Application.findAll();
+
+  if (!application){
+    return res.status(404).json({
+      message:"Application not found"
+    })
+  }
+  return res.status(200).json({
+    message:"All application are sucesfully given"
+  })
+}
+
+const myApplication=async(req,res)=>{
+  const userId=req.user.id
+
+  const application=await Application.findAll({where:{
+    userId:userId
+  }})
   if(!application){
     return res.status(404).json({
       message:"Application not found"
     })
   }
-
-  const deletedapplication=await Application
-
+  return res.status(200).json({
+    message:"My application fetched successfully",
+    application
+  })
 }
 
-module.exports={jobApply,updateApplicationStatus,deleteApplication}
+module.exports = { jobApply, updateApplicationStatus, deleteApplication,getApplication,myApplication }
--- routes/applicationRoutes.js
+++ routes/applicationRoutes.js
@@ -1,10 +1,13 @@
-const { jobApply, updateApplicationStatus } = require('../controller/applicationController');
+const { jobApply, updateApplicationStatus, deleteApplication, getApplication, myApplication } = require('../controller/applicationController');
 const { isAuthenticated, checkUserRole } = require('../middleware/userMiddleware');
 const { asyncError } = require("../services/asyncFunction");
 const router=require('express').Router();
 
-router.post('/applications/:jobId',asyncError(jobApply))
+router.post('/applicationcreate/:jobId',isAuthenticated,checkUserRole("jobseeker"),asyncError(jobApply))
 router.patch("/aplicationupdate/:id",isAuthenticated,checkUserRole("jobprovider"),asyncError(updateApplicationStatus))
-router.delete("/applicationdelete/:id")
+router.delete("/applicationdelete/:id",isAuthenticated,checkUserRole("jobseeker"),asyncError(deleteApplication))
+router.get('/getapplication',isAuthenticated,checkUserRole("jobprovider"),asyncError(getApplication))
+router.get("/myapplication",isAuthenticated,checkUserRole("jobseeker"),asyncError(myApplication))
+
 
 module.exports=router;
